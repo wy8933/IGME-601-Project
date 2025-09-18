235824e3090558b3787e91b3d87d7a44afced45d
wy8933 <112401719+wy8933@users.noreply.github.com>
2025-09-18 12:06:10 -0400

    Done with the basic item system

---
+++ Anomalous Studios/Assets/Scripts/Architecture/ItemSystem/ItemDefinitionSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace ItemSystem
+{
+    public enum ItemType { SingleUse, Tool, Other}
+
+    [CreateAssetMenu(fileName = "ItemDefinitionSO", menuName = "ItemSystem/ItemDefinitionSO")]
+    public abstract class ItemDefinitionSO  : ScriptableObject
+    {
+        [Tooltip("Definitions")]
+        public string itemName;
+        public int itemID;
+        public Sprite itemIcon;
+
+        [Header("Use Flags")]
+        public bool hasDurability = false;
+        public int durability = 1;
+        public float cooldownSeconds = 0f;
+
+        /// <summary>
+        /// Write the logic that you want the item to function, like this one is a item that simply prints a message
+        /// </summary>
+        /// <param name="user"> Who used the Item</param>
+        /// <param name="instance">The reference of the item instance</param>
+        /// <returns></returns>
+        public abstract bool Use(GameObject user, ref ItemInstance instance);
+    }
+}
---
+++ Anomalous Studios/Assets/Scripts/Items/ItemTester.cs
@@ -0,0 +1,25 @@
+using ItemSystem;
+using UnityEngine;
+
+public class ItemTester : MonoBehaviour
+{
+    public ItemDefinitionSO item;
+
+    private ItemInstance _instance;
+
+    void Start()
+    {
+        _instance.item = item;
+        _instance.Initialize();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _instance.TryUse(gameObject);
+            Debug.Log("Durability Left:"+_instance.durabilityLeft);
+            Debug.Log("Is the item on cooldown?" + _instance.IsOnCooldown);
+        }
+    }
+}
