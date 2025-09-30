667f89f5716f4cb0a386d6fe08d0b9d786298e49
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2025-09-29 22:23:01 -0400

    Working on paper interaction

--- Anomalous Studios/Assets/Scripts/InteractableObjects/Paper.cs
+++ Anomalous Studios/Assets/Scripts/InteractableObjects/Paper.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 
-public class Paper : MonoBehaviour
+public class Paper : Interaction
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Level _level;
+
+    private Renderer _renderer;
+
+    // TODO: Change the initialization of the first level to be dynamic, raise an event
+    // The level system is going to change pretty soon to accomadate new level box anyway
+    private static Level _currentLevel = Level.blue;
+
+    public void Start()
     {
-        
+        _renderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
+    {
+        base.Update();
+        
+
+    }
+
+    public override void Highlight()
+    {
+        // TODO: Replace with shader to highlight the item, or UI element to indicate it is interactable
+        print("Highlighting Paper");
+    }
+
+    protected override void Interact()
     {
         
+
     }
 }
