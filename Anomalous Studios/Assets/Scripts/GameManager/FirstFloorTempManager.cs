6c0eca6ecb6f5822bffc12ebd2690220bb4081bb
wy8933 <112401719+wy8933@users.noreply.github.com>
2025-10-03 17:17:26 -0400

    Created the first floor manager

---
+++ Anomalous Studios/Assets/Scripts/GameManager/FirstFloorTempManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FirstFloorTempManager : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (VariableConditionManager.Instance.Get("Trash") == "3") 
+        {
+            
+        }
+    }
+}
