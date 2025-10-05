e9c1455a026a81fb0b2584dea4db6e315a3c1ec9
wy8933 <112401719+wy8933@users.noreply.github.com>
2025-10-05 16:24:02 -0400

    Fixed bug with the game over menu

--- Anomalous Studios/Assets/Scripts/UI/GameOverUI.cs
+++ Anomalous Studios/Assets/Scripts/UI/GameOverUI.cs
@@ -7,6 +7,12 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] GameObject credits;
     bool isVisible = false;
 
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void BackGame()
     {
         SceneManager.LoadScene(0);
