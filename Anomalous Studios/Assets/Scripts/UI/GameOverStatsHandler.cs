7f8f198dd428483f3b02b9b0d781781e84fbcc57
Weijie Ye <wy8933@rit.edu>
2025-11-14 18:16:48 -0500

    Complete the Game Over UI

---
+++ Anomalous Studios/Assets/Scripts/UI/GameOverStatsHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+public class GameOverStatsHandler : MonoBehaviour
+{
+    public TMP_Text totalRuleBroken;
+    public TMP_Text totalTaskComplete;
+
+
+    public void Start()
+    {
+        totalRuleBroken.text = $"Total Number Of Policies Broken: {VariableConditionManager.Instance.Get("rule_broken_count:int")}" ;
+        totalTaskComplete.text = $"Total Number Of Tasks Completed: : {VariableConditionManager.Instance.Get("task_completed:int")}";
+    }
+
+}
