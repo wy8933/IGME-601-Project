1da974f7b6d13139ff10d5a77e2205bd82eab6e9
matthewfong3 <mxf9125@rit.edu>
2026-01-29 22:07:13 -0500

    improved elevator open button check
    implemented additional check before enabling elevator's open button
    
    player must now collect all policies AND view their handbook

--- Anomalous Studios/Assets/Scripts/PlayerController/PlayerJournal.cs
+++ Anomalous Studios/Assets/Scripts/PlayerController/PlayerJournal.cs
@@ -9,6 +9,10 @@ public class PlayerJournal : MonoBehaviour
 
     private EventBinding<LevelLoaded> _levelLoaded;
 
+    // Reference variables needed to check conditions before enabling the elevator's open button
+    private PlayerController _playerController;
+    private ElevatorController _elevatorController;
+
     // Getter Methods
     public bool GetInJournal() { return _inJournal; }
 
@@ -16,6 +20,10 @@ public class PlayerJournal : MonoBehaviour
     {
         // Transform.Find() searches for the handbook_UI even if its disabled
         handbook = GameObject.Find("MainUI").transform.Find("Handbook").GetComponent<Handbook_UI>();
+
+        // Find the references
+        _playerController = this.GetComponentInParent<PlayerController>();
+        _elevatorController = GameObject.FindGameObjectWithTag("Elevator").GetComponent<ElevatorController>();
     }
 
     public void ToggleHandbook()
@@ -32,5 +40,8 @@ public class PlayerJournal : MonoBehaviour
             UnityEngine.Cursor.lockState = CursorLockMode.Locked;
             UnityEngine.Cursor.visible = false;
         }
+
+        // Ensures that the player must view the handbook and have collected all policies in the elevator before elevator's open button is enabled
+        if (_elevatorController.NotesCount() <= 0 && _playerController.ViewedHandbook) _elevatorController.GetOpenButton().Enable();
     }
 }
